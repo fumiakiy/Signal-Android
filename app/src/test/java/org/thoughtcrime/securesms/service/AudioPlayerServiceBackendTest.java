579bf76f155e1f801a25ac3a830b5fa30186e0bb
Fumiaki Yoshimatsu <fumiakiy@gmail.com>
2020-06-18 17:30:44 -0400

    MOre tests.

--- app/src/test/java/org/thoughtcrime/securesms/service/AudioPlayerServiceBackendTest.java
+++ app/src/test/java/org/thoughtcrime/securesms/service/AudioPlayerServiceBackendTest.java
@@ -1,9 +1,12 @@
 package org.thoughtcrime.securesms.service;
 
+import static com.ibm.icu.impl.Assert.fail;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyBoolean;
 import static org.mockito.Matchers.anyObject;
 import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
@@ -12,18 +15,26 @@ import static org.mockito.Mockito.when;
 
 import android.app.Application;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.net.Uri;
 
 import android.service.autofill.FieldClassification.Match;
+import com.google.android.exoplayer2.C;
 import com.google.android.exoplayer2.ExoPlaybackException;
 import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.audio.AudioAttributes;
 import java.io.IOException;
+import java.lang.reflect.Field;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.mockito.stubbing.OngoingStubbing;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
@@ -33,6 +44,8 @@ import org.thoughtcrime.securesms.service.AudioPlayerServiceBackend.Command;
 @RunWith(RobolectricTestRunner.class)
 @Config(manifest = Config.NONE, application = Application.class)
 public class AudioPlayerServiceBackendTest {
+  private final FakeProximitySensor proximitySensor = new FakeProximitySensor();
+
   private AudioPlayerServiceBackend backend;
   private Player.EventListener playerEventListener;
 
@@ -42,8 +55,6 @@ public class AudioPlayerServiceBackendTest {
   private MediaPlayer mediaPlayer;
   @Mock
   private MediaPlayerFactory mediaPlayerFactory;
-  @Mock
-  private ProximitySensor proximitySensor;
   @Mock
   private ServiceInterface serviceInterface;
   @Mock
@@ -56,7 +67,7 @@ public class AudioPlayerServiceBackendTest {
     backend = new AudioPlayerServiceBackend(audioManager, mediaPlayerFactory, proximitySensor, serviceInterface, wakeLock);
   }
 
-  private void setupBound(Uri uri, double progress, AudioStateListener listener) {
+  private AudioPlayerServiceBackend.LocalBinder setupBound(Uri uri, double progress, AudioStateListener listener) {
     ArgumentCaptor<Player.EventListener> captor = ArgumentCaptor.forClass(Player.EventListener.class);
     when(mediaPlayerFactory.create(anyObject(), captor.capture(), anyBoolean())).thenReturn(mediaPlayer);
     Intent play = playCommand(uri, progress);
@@ -64,6 +75,8 @@ public class AudioPlayerServiceBackendTest {
     AudioPlayerServiceBackend.LocalBinder binder = (AudioPlayerServiceBackend.LocalBinder) backend.onBind(play);
     binder.setListener(listener);
     playerEventListener = captor.getValue();
+
+    return binder;
   }
 
   @Test
@@ -104,7 +117,6 @@ public class AudioPlayerServiceBackendTest {
     AudioStateListener mockListener = mock(AudioStateListener.class);
     setupBound(uri, 0, mockListener);
 
-    // Simulate player becomes ready
     playerEventListener.onPlayerStateChanged(true, Player.STATE_READY);
 
     verify(mockListener).onAudioStarted();
@@ -118,7 +130,7 @@ public class AudioPlayerServiceBackendTest {
     AudioStateListener mockListener = mock(AudioStateListener.class);
     setupBound(uri, 0, mockListener);
 
-    // Simulate player becomes ready
+    // Simulate playback starts
     playerEventListener.onPlayerStateChanged(true, Player.STATE_READY);
     // Simulate playback ends
     playerEventListener.onPlayerStateChanged(true, Player.STATE_ENDED);
@@ -141,6 +153,20 @@ public class AudioPlayerServiceBackendTest {
   }
 
   @Test
+  public void serviceStopsWhenPlaybackReachesAtEnd() {
+    Uri uri = Uri.parse("content://2");
+    AudioStateListener mockListener = mock(AudioStateListener.class);
+    setupBound(uri, 0, mockListener);
+
+    // Simulate playback starts
+    playerEventListener.onPlayerStateChanged(true, Player.STATE_READY);
+    // Simulate playback ends
+    playerEventListener.onPlayerStateChanged(true, Player.STATE_ENDED);
+
+    verify(serviceInterface).stop();
+  }
+
+  @Test
   public void mediaPlayerStartsAtSpecifiedProgress() {
     Uri uri = Uri.parse("content://3");
     double progress = 0.45;
@@ -153,6 +179,208 @@ public class AudioPlayerServiceBackendTest {
     playerEventListener.onPlayerStateChanged(true, Player.STATE_READY);
     verify(mediaPlayer).seekTo((long) (duration * progress));
   }
+
+  @Test
+  public void delayedStopTimerStartsWhenPlaybackStoppedFromExternal() {
+    Uri uri = Uri.parse("content://2");
+    AudioStateListener mockListener = mock(AudioStateListener.class);
+    AudioPlayerServiceBackend.LocalBinder binder = setupBound(uri, 0, mockListener);
+
+    doAnswer((InvocationOnMock invocation) -> {
+      playerEventListener.onPlayerStateChanged(true, Player.STATE_IDLE);
+      return null;
+    }).when(mediaPlayer).stop();
+
+    binder.stop();
+
+    verify(serviceInterface).stopDelayed();
+  }
+
+  @Test
+  public void resumingPlaybackStartsFromTheLastProgress() {
+    Uri uri = Uri.parse("content://2");
+    AudioStateListener mockListener = mock(AudioStateListener.class);
+    AudioPlayerServiceBackend.LocalBinder binder = setupBound(uri, 0, mockListener);
+
+    doAnswer((InvocationOnMock invocation) -> {
+      playerEventListener.onPlayerStateChanged(true, Player.STATE_IDLE);
+      return null;
+    }).when(mediaPlayer).stop();
+
+    // Mock progress
+    when(mediaPlayer.getDuration()).thenReturn(20000L);
+    when(mediaPlayer.getCurrentPosition()).thenReturn(1234L);
+    binder.stop();
+
+    backend.onStartCommand(resumeCommand());
+    playerEventListener.onPlayerStateChanged(true, Player.STATE_READY);
+
+    verify(mediaPlayer).seekTo(1234);
+  }
+
+  @Test
+  public void pausingPlaybackFromExternalUpdatesNotification() {
+    Uri uri = Uri.parse("content://2");
+    AudioStateListener mockListener = mock(AudioStateListener.class);
+    AudioPlayerServiceBackend.LocalBinder binder = setupBound(uri, 0, mockListener);
+
+    doAnswer((InvocationOnMock invocation) -> {
+      playerEventListener.onPlayerStateChanged(true, Player.STATE_IDLE);
+      return null;
+    }).when(mediaPlayer).stop();
+
+    binder.stop();
+
+    verify(serviceInterface).updateNotification(Command.PAUSE);
+  }
+
+  @Test
+  public void pausingPlaybackFromNotificationUpdatesNotification() {
+    Uri uri = Uri.parse("content://2");
+    AudioStateListener mockListener = mock(AudioStateListener.class);
+    setupBound(uri, 0, mockListener);
+
+    doAnswer((InvocationOnMock invocation) -> {
+      playerEventListener.onPlayerStateChanged(true, Player.STATE_IDLE);
+      return null;
+    }).when(mediaPlayer).stop();
+
+    backend.onStartCommand(pauseCommand());
+
+    verify(serviceInterface).updateNotification(Command.PAUSE);
+  }
+
+  @Test
+  public void resumingPlaybackFromExternalUpdatesNotification() {
+    Uri uri = Uri.parse("content://2");
+    AudioStateListener mockListener = mock(AudioStateListener.class);
+    AudioPlayerServiceBackend.LocalBinder binder = setupBound(uri, 0, mockListener);
+
+    doAnswer((InvocationOnMock invocation) -> {
+      playerEventListener.onPlayerStateChanged(true, Player.STATE_IDLE);
+      return null;
+    }).when(mediaPlayer).stop();
+
+    binder.stop();
+    backend.onStartCommand(playCommand(uri, 12));
+    verify(serviceInterface, times(2)).startForeground(Command.PLAY);
+  }
+
+  @Test
+  public void resumingPlaybackFromNotificationUpdatesNotification() {
+    Uri uri = Uri.parse("content://2");
+    AudioStateListener mockListener = mock(AudioStateListener.class);
+    AudioPlayerServiceBackend.LocalBinder binder = setupBound(uri, 0, mockListener);
+
+    doAnswer((InvocationOnMock invocation) -> {
+      playerEventListener.onPlayerStateChanged(true, Player.STATE_IDLE);
+      return null;
+    }).when(mediaPlayer).stop();
+
+    binder.stop();
+    backend.onStartCommand(resumeCommand());
+    verify(serviceInterface).updateNotification(Command.RESUME);
+  }
+
+  @Test
+  public void whenProximityUpdatesItPausesAndRestartsWithUpdatedAudioOutput() {
+    Uri uri = Uri.parse("content://4");
+    AudioStateListener mockListener = mock(AudioStateListener.class);
+    setupBound(uri, 0, mockListener);
+
+    // Simulate playback starts
+    playerEventListener.onPlayerStateChanged(true, Player.STATE_READY);
+    when(mediaPlayer.getPlaybackState()).thenReturn(Player.STATE_READY);
+    when(mediaPlayer.getAudioStreamType()).thenReturn(android.media.AudioManager.STREAM_MUSIC);
+
+    // Simulate proximity update
+    proximitySensor.listener.onSensorChanged(mockSensorEvent(2f));
+
+    // Simulate playback starts again
+    playerEventListener.onPlayerStateChanged(true, Player.STATE_READY);
+
+    verify(mediaPlayer).stop();
+    verify(mediaPlayer).release();
+    // earpiece is now set to true
+    verify(mediaPlayerFactory).create(eq(uri), anyObject(), eq(true));
+  }
+
+  @Test
+  public void whenProximityUpdatesButNotBelowThresholdItDoesNotDoAnything() {
+    Uri uri = Uri.parse("content://4");
+    AudioStateListener mockListener = mock(AudioStateListener.class);
+    setupBound(uri, 0, mockListener);
+
+    // Simulate playback starts
+    playerEventListener.onPlayerStateChanged(true, Player.STATE_READY);
+    when(mediaPlayer.getPlaybackState()).thenReturn(Player.STATE_READY);
+    when(mediaPlayer.getAudioStreamType()).thenReturn(android.media.AudioManager.STREAM_MUSIC);
+
+    // Simulate proximity update
+    proximitySensor.listener.onSensorChanged(mockSensorEvent(10f));
+
+
+    verify(mediaPlayer, never()).stop();
+    verify(mediaPlayer, never()).release();
+    // earpiece is now set to true
+    verify(mediaPlayerFactory, never()).create(eq(uri), anyObject(), eq(true));
+  }
+
+  @Test
+  public void whenProximityUpdatesAgainItPausesAndRestartsWithUpdatedAudioOutputAgain() {
+    Uri uri = Uri.parse("content://4");
+    AudioStateListener mockListener = mock(AudioStateListener.class);
+    setupBound(uri, 0, mockListener);
+
+    // Simulate playback starts
+    playerEventListener.onPlayerStateChanged(true, Player.STATE_READY);
+    when(mediaPlayer.getPlaybackState()).thenReturn(Player.STATE_READY);
+    when(mediaPlayer.getAudioStreamType()).thenReturn(android.media.AudioManager.STREAM_MUSIC);
+
+    // Simulate proximity update
+    proximitySensor.listener.onSensorChanged(mockSensorEvent(2f));
+
+    // Simulate playback starts again
+    playerEventListener.onPlayerStateChanged(true, Player.STATE_READY);
+
+    verify(mediaPlayer).stop();
+    verify(mediaPlayer).release();
+    // earpiece is now set to true
+    verify(mediaPlayerFactory).create(eq(uri), anyObject(), eq(true));
+    when(mediaPlayer.getAudioStreamType()).thenReturn(android.media.AudioManager.STREAM_VOICE_CALL);
+
+    // Simulate proximity update again
+    proximitySensor.listener.onSensorChanged(mockSensorEvent(200f));
+
+    // Simulate playback starts yet again
+    playerEventListener.onPlayerStateChanged(true, Player.STATE_READY);
+
+    verify(mediaPlayer).stop();
+    verify(mediaPlayer).release();
+    // earpiece is now set to true
+    verify(mediaPlayerFactory).create(eq(uri), anyObject(), eq(false));
+  }
+
+  @Test
+  public void closingNotificationStopService() {
+    Uri uri = Uri.parse("content://5");
+    AudioStateListener mockListener = mock(AudioStateListener.class);
+    AudioPlayerServiceBackend.LocalBinder binder = setupBound(uri, 0, mockListener);
+
+    doAnswer((InvocationOnMock invocation) -> {
+      playerEventListener.onPlayerStateChanged(true, Player.STATE_IDLE);
+      return null;
+    }).when(mediaPlayer).stop();
+
+    backend.onStartCommand(closeCommand());
+
+    verify(mediaPlayer).release();
+    verify(serviceInterface).stop();
+  }
+
+  /*
+   * Helper methods
+   */
 
   private Intent playCommand(Uri uri, double progress) {
     Intent intent = new Intent();
@@ -173,4 +401,51 @@ public class AudioPlayerServiceBackendTest {
     intent.putExtra(AudioPlayerServiceBackend.COMMAND_EXTRA, AudioPlayerServiceBackend.Command.RESUME);
     return intent;
   }
+
+  private Intent closeCommand() {
+    Intent intent = new Intent();
+    intent.putExtra(AudioPlayerServiceBackend.COMMAND_EXTRA, AudioPlayerServiceBackend.Command.CLOSE);
+    return intent;
+  }
+
+  private SensorEvent mockSensorEvent(float value) {
+    try {
+      SensorEvent sensorEvent = mock(SensorEvent.class);
+
+      Field sensorField = SensorEvent.class.getField("sensor");
+      sensorField.setAccessible(true);
+      Sensor sensor = mock(Sensor.class);
+      when(sensor.getType()).thenReturn(Sensor.TYPE_PROXIMITY);
+      sensorField.set(sensorEvent, sensor);
+
+      Field valuesField = SensorEvent.class.getField("values");
+      valuesField.setAccessible(true);
+      valuesField.set(sensorEvent, new float[]{value});
+      return sensorEvent;
+
+    } catch (NoSuchFieldException | IllegalAccessException e) {
+      fail(e);
+      return null;
+    }
+  }
+
+  private static class FakeProximitySensor implements ProximitySensor {
+    SensorEventListener listener;
+    @Override
+    public float getMaximumRange() {
+      return 50000f;
+    }
+
+    @Override
+    public void registerListener(SensorEventListener listener, int samplingPeriodUs) {
+      this.listener = listener;
+    }
+
+    @Override
+    public void unregisterListener(SensorEventListener listener) {
+      this.listener = null;
+    }
+  }
+
 }
+
